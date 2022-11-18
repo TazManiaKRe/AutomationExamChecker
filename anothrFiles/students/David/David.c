4220c1ffbca85922cf33d9fbc406e264dc5e3270
TazMania <74743286+TazManiaKRe@users.noreply.github.com>
2022-11-18 02:02:00 +0200

    Add files via upload

---
+++ anothrFiles/students/David/David.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char* argv[]){
+int a,b;
+printf("please enter first number: \n");
+scanf("%d", &a);
+printf("please enter scond number: \n");
+scanf("%d", &b);	
+printf("the sum is: \n");
+printf("%d\n", a+b+1);
+return 0;
+}
